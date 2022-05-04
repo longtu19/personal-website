117ee4460f959a2b981ddbb1ebddf518e5bde941
Long Huynh <longbhuynh019@gmail.com>
2022-05-03 23:48:24 -0400

    designed awards components

--- my-website/src/components/about/About.jsx
+++ my-website/src/components/about/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
                     </div>
 
                     <p>I love coding stuff!</p>
-                    <a href = "#contact" className = "btn btn-primary">Let's Talk</a>
+                    <a href = "#contact" className = "btn btn-about">Let's Talk</a>
 
  
                 </div>
