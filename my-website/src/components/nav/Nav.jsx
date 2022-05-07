46fb064969e6dc977a10a18c23a0e1e595bf7cae
Long Huynh <longbhuynh019@gmail.com>
2022-05-06 21:33:17 -0400

    updated skills comp

--- my-website/src/components/nav/Nav.jsx
+++ my-website/src/components/nav/Nav.jsx
@@ -10,7 +10,7 @@ const nav = () => {
     const [activeNav, setActiveNav] = useState('#')
     return (
         <nav>
-            <a href = "#" onClick = {() => setActiveNav('#')} className = {activeNav === '#' ? 'active': ''}><ImHome/></a>
+            <a href = "#" onClick = {() => setActiveNav('#')} className = {activeNav === '#' ? 'active': ''}><ImHome color = "#1B9FA8"/></a>
             <a href = "#about" onClick = {() => setActiveNav('#about')}  className = {activeNav === '#about' ? 'active' : ''}><FaUser/></a>
             <a href = "#skills" onClick = {() => setActiveNav('#skills')}  className = {activeNav === '#skills' ? 'active' : ''}><MdWorkspaces/></a>
             <a href = "#experience" onClick = {() => setActiveNav('#experience')}  className = {activeNav === '#experience' ? 'active' : ''}><RiServiceFill/></a>
